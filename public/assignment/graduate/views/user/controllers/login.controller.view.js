4dce2a151a23e9033ed6577b7d3b1714b22fd735
maheshkumar95 <maheshkumar95@ccs.neu.edu>
2017-05-26 12:04:35 -0400

    3.7

--- public/assignment/graduate/views/user/controllers/login.controller.view.js
+++ public/assignment/graduate/views/user/controllers/login.controller.view.js
@@ -2,30 +2,17 @@
     angular
         .module("WAM")
         .controller("loginController",loginController);
-    function loginController() {
+
+    function loginController($location,userService) {
         var model=this;
-        var users = [
-            {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-            {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-            {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
-        ];
 
         model.login = login;
 
         function login(username, password) {
-            var found = false;
-            for (var u in users) {
-                var user = users[u];
-                if (user.username === username &&
-                    user.password === password) {
-                    found = true;
-                    break;
-                }
-            }
-            if (found) {
-                model.message = "welcome " + username;
+            var found = userService.findUserByCredentials(username,password);
 
+            if (found!==null) {
+                $location.url("/profile/" +found._id);
             }
             else {
                 model.message = "sorry." + username + "not found. please try again";
