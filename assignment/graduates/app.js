273b2440ddc983db75b8cb76ab9a49d32e33d378
maheshkumar95 <maheshkumar95@ccs.neu.edu>
2017-06-22 20:37:11 -0400

    home page update

--- assignment/graduates/app.js
+++ assignment/graduates/app.js
@@ -4,7 +4,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
 
-var connectionString = 'mongodb://127.0.0.1:27017/test';
+var connectionString = 'mongodb://127.0.0.1:27017/webdev_summer1_2017';
 
 if(process.env.MLAB_USERNAME) {
     connectionString = process.env.MLAB_USERNAME + ":" +
